4f6aff6a757785386774526dbc591d8954ccca65
Javier <javi_roig@hotmail.es>
2021-10-17 12:55:43 +0200

    feature: contacto frontend done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './app.css';
 import Sidebar from './components/sidebar/sidebar';
 import { SkillsList } from './components/skills/SkillsList';
 import { Portfolio } from './components/portfolio/Portfolio';
+import { Contacto } from './components/contacto/Contacto';
 
 function App () {
 
@@ -12,6 +13,7 @@ function App () {
                 <Introduccion />
                 <SkillsList />
                 <Portfolio />
+                <Contacto />
                 <Sidebar />
             </main>
         </div>
---
+++ src/components/contacto/Contacto.jsx
@@ -0,0 +1,34 @@
+
+import './contacto.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export const Contacto = () => {
+
+    return (
+        <div id="contacto" className="contacto">
+
+            <Form>
+                <span>Contáctame</span>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email de contacto</Form.Label>
+                    <Form.Control type="email" placeholder="Introduce un email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Motivo de contacto</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                    />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                        Submit
+                </Button>
+            </Form>
+        </div>
+    );
+
+};
