f5f4d64be397acce829090c75de3e78930c11709
Javier <javi_roig@hotmail.es>
2021-10-17 12:00:45 +0200

    Added introduccion

---
+++ src/components/introducción/Introduccion.jsx
@@ -0,0 +1,16 @@
+import './introduccion.css';
+
+export const Introduccion = () => {
+
+    return (
+        <>
+            <div id="inicio" className="introduccion">
+                <div className="descripcion">
+                    <span className="titulo">Desarrollador Frontend</span>
+                    <p>Pariatur quis consectetur excepteur amet ad laboris voluptate anim aliqua in. Amet id quis ad aliquip duis eu quis labore consectetur laborum commodo. Labore qui reprehenderit eiusmod proident officia veniam sunt laboris. Aliqua ullamco veniam fugiat non aute veniam esse ex nostrud occaecat. Enim nulla laborum ut aute excepteur irure nostrud velit duis nulla duis sint. Elit qui sit officia excepteur mollit ullamco ex ipsum. Quis cupidatat non officia est nostrud culpa.</p>
+                </div>
+            </div>
+        </>
+    );
+
+};
