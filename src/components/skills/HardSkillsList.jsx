2247417b097f45869695782170587e11a2b406f1
Javier <javi_roig@hotmail.es>
2021-10-17 16:19:36 +0200

    fix: skill names fixed

--- src/components/skills/HardSkillsList.jsx
+++ src/components/skills/HardSkillsList.jsx
@@ -6,13 +6,13 @@ export const HardSkillsList = () => {
     return (
         <div className="hardSkillList">
             <Skill logo={ html } nombre="HTML5" nivel="Nivel intermedio" />
-            <Skill logo={ css } nombre="React" nivel="Nivel principiante" />
-            <Skill logo={ javascript } nombre="React" nivel="Nivel intermedio" />
+            <Skill logo={ css } nombre="CSS" nivel="Nivel principiante" />
+            <Skill logo={ javascript } nombre="JavaScript" nivel="Nivel intermedio" />
             <Skill logo={ react } nombre="React" nivel="Nivel intermedio" />
-            <Skill logo={ express } nombre="React" nivel="Nivel intermedio" />
+            <Skill logo={ express } nombre="Express" nivel="Nivel intermedio" />
             <Skill logo={ git } nombre="Git" nivel="Nivel intermedio" />
             <Skill logo={ cpp } nombre="cpp" nivel="Nivel principiante" />
-            <Skill logo={ java } nombre="React" nivel="Nivel principiante" />
+            <Skill logo={ java } nombre="Java" nivel="Nivel principiante" />
         </div>
     );
 
