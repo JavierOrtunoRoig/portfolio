5c663c033ca00497aa075ec66cf03c02cadb1bf4
Javier <javi_roig@hotmail.es>
2021-10-29 22:08:06 +0200

    feature: added email contact

--- src/components/contacto/Contacto.jsx
+++ src/components/contacto/Contacto.jsx
@@ -4,27 +4,57 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import emailjs from 'emailjs-com';
+import sweetAlert from 'sweetalert2';
 
 export const Contacto = () => {
 
+    const handleSubmit = ( e ) => {
+
+        e.preventDefault();
+
+        emailjs.sendForm( 'service_portfolio', 'template_b735ig5', e.target, 'user_dcKRqtgyEaMyVf3IQuapM' )
+            .then( ( result ) => {
+
+                console.log( result );
+                alert( '¡Enhorabuena, el correo se ha neviado correctamente!' );
+
+            }, ( error ) => {
+
+                console.log( error );
+                alert( 'Parece que algo ha salido mal.' );
+
+            });
+
+    };
+
     return (
         <div id="contacto" className="contacto">
-            <Form>
+            <Form onSubmit={ handleSubmit }>
                 <span>Contáctame</span>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control
+                        name="name"
+                    />
+                </Form.Group>
+
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email de contacto</Form.Label>
-                    <Form.Control type="email" placeholder="Introduce un email" />
+                    <Form.Control name="email" type="email" placeholder="Introduce tu email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Motivo de contacto</Form.Label>
                     <Form.Control
+                        name="message"
                         as="textarea"
                     />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
-                        Submit
+                    Submit
                 </Button>
             </Form>
         </div>
