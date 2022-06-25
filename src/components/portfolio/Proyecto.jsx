cde28676d42fe1d862c286f3d2155aa2e77b02ac
Kelsier <javi_roig@hotmail.es>
2022-06-25 13:46:01 +0200

    Subida laptop

--- src/components/portfolio/Proyecto.jsx
+++ src/components/portfolio/Proyecto.jsx
@@ -1,10 +1,17 @@
 import PropTypes from 'prop-types';
+import github from '../../assets/github.png';
 
 const Proyecto = ({ foto }) => {
 
     return (
         <div>
-            <img src={ foto } />
+            <div className="host">
+                <div className="prueba">
+                    <div className="github-container">
+                        <a href="#" className="github-button"><img src={ github } ></img></a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 
